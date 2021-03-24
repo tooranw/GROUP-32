1a029b8d3443d6099399d9ba3227ec9973ca20c6
Jerry Hu <huyifei2018516@gmail.com>
2021-03-23 18:00:19 -0700

    try

--- scripts/firebase_api_group32.js
+++ scripts/firebase_api_group32.js
@@ -4,6 +4,7 @@
 //--------------------------------------------------- 
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
     apiKey: "AIzaSyDgMtvOdW1Bf6GKfIpGqcx40fCSashDGFA",
     authDomain: "group32database-2cf96.firebaseapp.com",
@@ -12,8 +13,8 @@ var firebaseConfig = {
     messagingSenderId: "399508154319",
     appId: "1:399508154319:web:94c92001b97239d39f14ba",
     measurementId: "G-NWBTKMQK73"
-  };
-
+};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 const db = firebase.firestore();
